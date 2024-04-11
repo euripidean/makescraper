94f42ff0d58ae348ec3e506ad7c9b4b9d38c5c98
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2024-04-11 14:04:46 -0700

    Writing listings to JSON file

--- scrape.go
+++ scrape.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/gocolly/colly"
 )
 
 type Listing struct {
-	Title string
-	Price string
-	Location string
-	Link string
+	Title string `json:"title"`
+	Price string `json:"price"`
+	Location string `json:"location"`
+	Link string `json:"link"`
 }
 
 // main() contains code adapted from example found in Colly's docs:
@@ -18,23 +22,42 @@ type Listing struct {
 func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
+
+	// set an empty slice of listing structs
 	listings := []Listing{}
 
 	c.OnHTML("li.cl-static-search-result", func(e *colly.HTMLElement) {
-	
+		// scrape the data
 		title := e.ChildText("div.title")
 		price := e.ChildText("div.price")
 		location := e.ChildText("div.location")
 		link := e.ChildAttr("a", "href")
-		
+		// create a new listing struct
 		listing := Listing{title, price, location, link}
+		// append the listing struct to the slice
 		listings = append(listings, listing)
 	})
 
 	c.OnScraped(func(r *colly.Response) {
-		fmt.Println(listings)
-	})
+		// Once everything is scraped, turn the slice into a JSON string
+		listingsJSON, err := json.Marshal(listings)
+		if err != nil {
+			log.Fatalf("Failed to encode JSON: %v", err)
+	}
+	// create the file
+	file, err := os.Create("listings.json")
+	if err != nil {
+		log.Fatalf("Failed to create file: %v", err)
+	}
+	defer file.Close()
 
+	// add the JSON to the file
+	writer := bufio.NewWriter(file)
+	_, err = writer.WriteString(string(listingsJSON))
+	if err != nil {
+		log.Fatalf("Failed to write to file: %v", err)
+	}
+	})
 
 	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
